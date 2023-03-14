870d88e2f563b98e01e93f8ecd0b2d77124e6984
Agustin Sbernini <fliasbernini@hotmail.com>
2023-03-14 11:08:38 -0300

    Prueba Dossier

---
+++ SignalREjemplo/SignalREjemplo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignalREjemplo.MvcApplication" Language="C#" %>
---
+++ SignalREjemplo/SignalREjemplo/Models/DossierHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace SignalREjemplo.Models
+{
+    public class DossierHub : Hub
+    {
+        public void Alertar(int cantDossier)
+        {
+            Thread.Sleep(cantDossier * 100);
+            Clients.Caller.DarAlerta();
+        }
+    }
+}
